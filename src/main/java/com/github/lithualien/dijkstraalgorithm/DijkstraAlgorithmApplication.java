e663201e9e88e619096738aeab87cf9604b46a8a
lithualien <tomas.dominauskas@protonmail.com>
2020-04-02 15:43:12 +0300

    Spring boot starting files

---
+++ src/main/java/com/github/lithualien/dijkstraalgorithm/DijkstraAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.github.lithualien.dijkstraalgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DijkstraAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DijkstraAlgorithmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/lithualien/dijkstraalgorithm/DijkstraAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lithualien.dijkstraalgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DijkstraAlgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
